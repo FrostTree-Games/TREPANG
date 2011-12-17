81dfb769c38ff179e065138de8e36b3634ce485b
Daniel Savage <dansvg@gmail.com>
2011-12-16 16:04:14 -0800

    created a template SDL test file to check if regular SDL library is working.
    Still need to check for:
    - SDL_mixer
    - SDL_image
    - SDL_ttf

--- main.c
+++ main.c
@@ -1,8 +1,51 @@
 #include <stdio.h>
 
-int main(int argc, char** argv)
+#include "SDL/SDL.h"
+
+// this data type will be used for the game's screen
+SDL_Surface* screen;
+
+int gameBackendSetup()
 {
-	printf("Hello world!\n");
+	// initalizes the SDL library
+	if (SDL_Init (SDL_INIT_EVERYTHING) == -1)
+	{
+		perror("Error on SDL_Init. Check your something");
+		return -1;
+	}
+	
+	// initalizes the screen pointer to the screen
+	if ((screen = SDL_SetVideoMode( 320, 240, 32, SDL_SWSURFACE)) == NULL)
+	{
+		perror("Error on intialzing video memory.");
+		return -1;
+	}
+
+	return 0;
+}
+
+int gameBackendClose()
+{
+	// frees the screen pointer and closes SDL
+	SDL_Quit();
+
+	return 0;
+}
+
+int main(int argc, char* argv[])
+{
+	// setup, exit if fail
+	if (gameBackendSetup() == -1)
+	{
+		return -1;
+	}
+
+	SDL_Flip(screen);
+	SDL_Delay(2000);
+
+	// cleanup
+	gameBackendClose();
+
 	return 0;
 }
 
